48d7a48d1afe059cd10e6cb044b380e7c9a2ac4b
fifoteam <haitaox2008@163.com>
2018-03-22 10:08:29 +0800

    AUTO SAVED BY BAT FILE

--- git/git_push_only.bat
+++ git/git_push_only.bat
@@ -2,5 +2,5 @@ echo off
 echo %1
 ::½øÈëÄ¿Â¼
 cd /d %1
-git commit -m "AUTO SAVED BY BAT FILE"
+git commit -a -m "AUTO SAVED BY BAT FILE"
 git push -u origin master
---
+++ ise/ise32.bat
@@ -0,0 +1,1 @@
+call D:\Tools\Xilinx\14.7\ISE_DS\settings32.bat d:\tools\Xilinx\14.7\ISE_DS\ISE\bin\nt\ise.exe
