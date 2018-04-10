cb393f6896f2fe166d3b24404a0cb4864f5af20b
fifoteam <haitaox2008@163.com>
2018-04-10 13:58:10 +0800

    AUTO SAVED BY BAT FILE

--- app/_impact.cmd
+++ app/_impact.cmd
@@ -7,11 +7,6 @@ Identify -inferir
 identifyMPM 
 assignFile -p 1 -file "F:/DAHENG/rd_mars_series/PD/product_stage/mars-1230-9gx-p/develop_stage/burn_file/3rd/full/bin/both_fac/MARS-1230-9GX-P/download.bit"
 Program -p 1 
-setCable -port auto
-Identify -inferir 
-identifyMPM 
-assignFile -p 1 -file "F:/DAHENG/rd_mars_series/PD/product_stage/mars-1230-9gx-p/develop_stage/burn_file/3rd/full/bin/both_fac/MARS-1230-9GX-P/FacProgFPGA_V01.03.001.001.bit"
-Program -p 1 
 setMode -bs
 setMode -bs
 setMode -ss
