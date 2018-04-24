f83a14b7eb5b3fca4a43962f14dcd1d8b013f46a
fifoteam <haitaox2008@163.com>
2018-04-24 08:50:13 +0800

    AUTO SAVED BY BAT FILE

---
+++ boot/back_up_fifoteam.bat
@@ -0,0 +1,52 @@
+
+echo off
+::xcopy  /e  复制目录和子目录
+::xcopy  /h  也复制隐藏文件和系统文件
+::xcopy  /y  取消提示已确认覆盖现有文件
+
+::rd	/S	删除目录树时不要求确认
+::rd	/Q	安静模式
+::RD /S /Q \?%1  删除任意文件夹
+
+
+::配置文件上传到SVN
+@ cd	f:\FifoTeam\bat_tools  >nul
+git add *
+git commit -a -m "AUTO SAVED BY BAT FILE"
+git push -u origin master
+
+@ cd	F:\FifoTeam\ahk-tools  >nul
+git add *
+git commit -a -m "AUTO SAVED BY BAT FILE"
+git push -u origin master
+
+
+@ cd	F:\FifoTeam\js-tools  >nul
+git add *
+git commit -m "AUTO SAVED BY BAT FILE"
+git push -u origin master
+
+@ cd	F:\FifoTeam\python-tools  >nul
+git add *
+git commit -a -m "AUTO SAVED BY BAT FILE"
+git push -u origin master
+
+@ cd	F:\FifoTeam\rtl-tools  >nul
+git add *
+git commit -m "AUTO SAVED BY BAT FILE"
+git push -u origin master
+
+echo "fifoteam git add & commit done!"
+
+@ping 127.0.0.1 -n 2 >nul
+echo "5"
+@ping 127.0.0.1 -n 2 >nul
+echo "4"
+@ping 127.0.0.1 -n 2 >nul
+echo "3"
+@ping 127.0.0.1 -n 2 >nul
+echo "2"
+@ping 127.0.0.1 -n 2 >nul
+echo "1"
+@ping 127.0.0.1 -n 2 >nul
+echo "0"
