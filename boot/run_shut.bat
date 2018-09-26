a9f302abfcea6558abe2ae5093ceead7c6a93855
fifoteam <haitaox2008@163.com>
2018-09-26 09:09:40 +0800

    AUTO SAVED BY BAT FILE

--- boot/run_shut.bat
+++ boot/run_shut.bat
@@ -6,6 +6,21 @@
 echo "daily plan"
 call f:\fifoteam\bat_tools\daily_plan\xht_plan_sum.bat
 
+echo "taskkill wiz"
+taskkill /f /im wiz.exe
+
+echo "taskkill 263em"
+taskkill /f /im 263em.exe
+
+echo "taskkill shadowsocks"
+taskkill /f /im Shadowsocks.exe
+
+echo "taskkill uedit32"
+taskkill /f /im uedit32.exe
+
+echo "taskkill TOTALCMD"
+taskkill /f /im TOTALCMD.exe
+
 echo "zme"
 call f:\fifoteam\bat_tools\boot\back_up_zme.bat
 
