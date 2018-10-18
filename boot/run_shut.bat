f0f2002e20783965e0ccefe371833dd518a08985
fifoteam <haitaox2008@163.com>
2018-10-18 13:23:15 +0800

    AUTO SAVED BY BAT FILE

--- boot/run_shut.bat
+++ boot/run_shut.bat
@@ -6,23 +6,8 @@
 echo "daily plan"
 call f:\fifoteam\bat_tools\daily_plan\xht_plan_sum.bat
 
-echo "taskkill ALTRun"
-taskkill /f /im ALTRun.exe
-
-echo "taskkill wiz"
-taskkill /f /im wiz.exe
-
-echo "taskkill 263em"
-taskkill /f /im 263em.exe
-
-echo "taskkill shadowsocks"
-taskkill /f /im Shadowsocks.exe
-
-echo "taskkill uedit32"
-taskkill /f /im uedit32.exe
-
-echo "taskkill TOTALCMD"
-taskkill /f /im TOTALCMD.exe
+echo "task_kill"
+call f:\fifoteam\bat_tools\boot\task_kill.bat
 
 echo "zme"
 call f:\fifoteam\bat_tools\boot\back_up_zme.bat
@@ -36,7 +21,8 @@ rem @ start	"" "D:\Program Files\Nutstore\Nutstore.exe"
 echo "weiyunsync"
 @ start	"" "D:\Program Files (x86)\Tencent\WeiyunSync\WeiyunSync\Bin\weiyunsync.exe"
 
-shutdown -s -t 120
+rem 延时5分钟关机，用于同步数据
+shutdown -s -t 300
 
 ::pause
 
---
+++ boot/task_kill.bat
@@ -0,0 +1,23 @@
+
+@ echo off
+::关机流程
+
+
+echo "taskkill ALTRun"
+taskkill /f /im ALTRun.exe
+
+echo "taskkill wiz"
+taskkill /f /im wiz.exe
+
+echo "taskkill 263em"
+taskkill /f /im 263em.exe
+
+echo "taskkill shadowsocks"
+taskkill /f /im Shadowsocks.exe
+
+echo "taskkill uedit32"
+taskkill /f /im uedit32.exe
+
+echo "taskkill TOTALCMD"
+taskkill /f /im TOTALCMD.exe
+
