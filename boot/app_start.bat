b190f2037f46b03d94d0813e893ad793942ba270
fifoteam <haitaox2008@163.com>
2019-01-16 11:32:16 +0800

    AUTO SAVED BY BAT FILE

---
+++ boot/app_start.bat
@@ -0,0 +1,45 @@
+
+@ echo off
+::关机流程
+
+
+echo "shadowsocks"
+@ start	"" "D:\Tools\xht_tools\Shadowsocks-2.5.8\Shadowsocks.exe"
+
+
+rem echo "Microsoft Security Client"
+rem @ start	"" "c:\Program Files\Microsoft Security Client\msseces.exe" -hide -runkey
+
+echo "autorun.ahk"
+@ start	"" "f:\fifoteam\ahk-tools\autorun\autorun.ahk"
+::echo "Ditto"
+::@ start	"" "D:\Tools\xht_tools\Ditto\Ditto.exe"
+::echo "dexpot"
+::@ start	"" "D:\Tools\xht_tools\dexpot_1610_portable_r2373\dexpot.exe"
+echo "263em"
+@ start	"" "D:\Program Files (x86)\263\263 Enterprise Messenger\263em.exe"
+
+::echo "Foxmail"
+::@ start	"" "D:\Program Files\Foxmail 7.0\Foxmail.exe" /MIN
+echo "Thunderbird"
+@ start	"" "D:\Program Files (x86)\Mozilla Thunderbird\thunderbird.exe" /MIN
+echo "Wiz"
+@ start	"" "D:\Program Files (x86)\WizNote\Wiz.exe"
+::echo "Evernote"
+::@ start	"" "D:\Program Files (x86)\Evernote\Evernote\Evernote.exe"
+echo "TOTALCMD64"
+@ start	"" "D:\Tools\totalcmd\TOTALCMD.EXE"
+::echo "everything"
+::@ start	"" "D:\Tools\Everything\Everything.exe"
+::echo "CCleaner"
+::@ start	"" "d:\Tools\xht_tools\CCleaner Professional Portable\CCleaner64.exe" /MIN
+echo "strokeit"
+@ start	"" "D:\Tools\xht_tools\TCB Networks\StrokeIt\strokeit.exe"
+
+
+
+::echo "flux"
+::@ start	"" "C:\Users\xht\AppData\Local\FluxSoftware\Flux\flux.exe"
+
+::echo "Wunderlist"
+::@ start	"" C:\Users\xht\AppData\Local\Wunderlist\Wunderlist.exe"
--- boot/run_init.bat
+++ boot/run_init.bat
@@ -2,49 +2,32 @@
 @ echo off
 ::开机启动项
 
-echo "shadowsocks"
-@ start	"" "D:\Tools\xht_tools\Shadowsocks-2.5.8\Shadowsocks.exe"
+echo "ccauto"
+call f:\FifoTeam\bat-tools\app\ccauto.bat
 
-
-rem echo "Microsoft Security Client"
-rem @ start	"" "c:\Program Files\Microsoft Security Client\msseces.exe" -hide -runkey
-
-echo "autorun.ahk"
-@ start	"" "f:\fifoteam\ahk-tools\autorun\autorun.ahk"
-::echo "Ditto"
-::@ start	"" "D:\Tools\xht_tools\Ditto\Ditto.exe"
-::echo "dexpot"
-::@ start	"" "D:\Tools\xht_tools\dexpot_1610_portable_r2373\dexpot.exe"
-echo "263em"
-@ start	"" "D:\Program Files (x86)\263\263 Enterprise Messenger\263em.exe"
-
-::echo "Foxmail"
-::@ start	"" "D:\Program Files\Foxmail 7.0\Foxmail.exe" /MIN
-echo "Thunderbird"
-@ start	"" "D:\Program Files (x86)\Mozilla Thunderbird\thunderbird.exe" /MIN
-echo "Wiz"
-@ start	"" "D:\Program Files (x86)\WizNote\Wiz.exe"
-::echo "Evernote"
-::@ start	"" "D:\Program Files (x86)\Evernote\Evernote\Evernote.exe"
-echo "TOTALCMD64"
-@ start	"" "D:\Tools\totalcmd\TOTALCMD.EXE"
-::echo "everything"
-::@ start	"" "D:\Tools\Everything\Everything.exe"
-::echo "CCleaner"
-::@ start	"" "d:\Tools\xht_tools\CCleaner Professional Portable\CCleaner64.exe" /MIN
-echo "strokeit"
-@ start	"" "D:\Tools\xht_tools\TCB Networks\StrokeIt\strokeit.exe"
+rem 系统清理 5s 之后，再开始启动
+@ping 127.0.0.1 -n 2 >nul
+echo "5"
+@ping 127.0.0.1 -n 2 >nul
+echo "4"
+@ping 127.0.0.1 -n 2 >nul
+echo "3"
+@ping 127.0.0.1 -n 2 >nul
+echo "2"
+@ping 127.0.0.1 -n 2 >nul
+echo "1"
+@ping 127.0.0.1 -n 2 >nul
+echo "0"
 
 
 
-::echo "flux"
-::@ start	"" "C:\Users\xht\AppData\Local\FluxSoftware\Flux\flux.exe"
+echo "daily plan"
+call f:\fifoteam\bat-tools\daily_plan\xht_plan_plan.bat
 
-::echo "Wunderlist"
-::@ start	"" C:\Users\xht\AppData\Local\Wunderlist\Wunderlist.exe"
+echo "app_start"
+call f:\fifoteam\bat-tools\boot\app_start.bat
 
 
-::echo "taskkill GWX"
-::taskkill /f /im GWX.exe
+
 
 ::pause
