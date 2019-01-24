abd7b296aa5aca82a3984b7bab98648ad550db94
fifoteam <haitaox2008@163.com>
2019-01-24 19:31:00 +0800

    AUTO SAVED BY BAT FILE

--- boot/task_kill.bat
+++ boot/task_kill.bat
@@ -4,24 +4,24 @@
 
 
 echo "taskkill ALTRun"
-taskkill /f /im ALTRun.exe
+taskkill /im ALTRun.exe
 
 echo "taskkill wiz"
-taskkill /f /im wiz.exe
+taskkill /im wiz.exe
 
 echo "taskkill 263em"
-taskkill /f /im 263em.exe
+taskkill /im 263em.exe
 
 echo "taskkill shadowsocks"
-taskkill /f /im Shadowsocks.exe
+taskkill /im Shadowsocks.exe
 
 echo "taskkill uedit32"
-taskkill /f /im uedit32.exe
+taskkill /im uedit32.exe
 
 echo "taskkill TOTALCMD"
-taskkill /f /im TOTALCMD.exe
+taskkill /im TOTALCMD.exe
 
 
 echo "taskkill thunderbird"
-taskkill /f /im thunderbird.exe
+taskkill /im thunderbird.exe
 
