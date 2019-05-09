53f40485f5e565f59d2b724b582bfa039ee147e6
fifoteam <haitaox2008@163.com>
2019-05-09 09:51:08 +0800

    AUTO SAVED BY BAT FILE

---
+++ daily_plan/xht_send_email.bat
@@ -0,0 +1,2 @@
+@ set day=%date:~0,4%%date:~5,2%%date:~8,2%
+"D:\Program Files (x86)\Mozilla Thunderbird\thunderbird.exe" -compose "to=zhouzy@daheng-imaging.com,subject=邢海涛项目管理日常工作记录【%day%】,attachment='file:///f:/DAHENG/hw_schedule/daily_plan/fpga/xinghaitao/2019年项目经理活动表_邢海涛.xlsx '"
